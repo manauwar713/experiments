a759ae18205ad8afd2b3c51eda8317fb7d82c2e7
manauwar713 <malamsr25@gmail.com>
2024-02-06 19:49:40 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Contains experiments for Mtech project',
+    author='Manauwar alam',
+    license='',
+)
